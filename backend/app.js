24e659833cef8430930333049ea6929e82ab877e
dev.abhinavnair <dev.abhinavnair@gmail.com>
2026-07-10 10:19:55 +0530

    my changes to create conflict.

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const app = express();
 
 app.use("/", (req,res) => {
-    res.send("hello from server");
+    res.send("hello from server, this project was made by both Abhinav and Nafih. We will never give you up, we will never let you down :)");
 })
 app.listen(3000, () => {
     console.log("server is running at:http://localhost:3000")
